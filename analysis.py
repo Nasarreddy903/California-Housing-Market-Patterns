5b59ee33831db4171e2eb141e1e14ec5f2e484d7
Nasarreddy903 <nasarreddy903@gmail.com>
2024-11-04 15:19:53 +0300

    add distribution plot

--- analysis.py
+++ analysis.py
@@ -21,3 +21,15 @@ def load_and_clean_data() -> pd.DataFrame:
     df['PRICE'] = housing.target
     
     return df
+
+def create_distribution_plot(df: pd.DataFrame) -> plt.Figure:
+    """
+    Create a histogram showing the distribution of house prices.
+    Returns matplotlib figure object.
+    """
+    plt.figure(figsize=(10, 6))
+    sns.histplot(data=df, x='PRICE', bins=50)
+    plt.title('Distribution of House Prices in California')
+    plt.xlabel('Price (100k USD)')
+    plt.ylabel('Count of Houses')
+    return plt.gcf()
